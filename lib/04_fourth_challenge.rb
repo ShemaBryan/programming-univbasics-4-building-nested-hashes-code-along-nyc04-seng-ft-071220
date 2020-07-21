d6cfb971e69fbdc16881ac667f9dce0572593b1c
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-21 14:21:28 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -2,27 +2,28 @@ def fourth_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age:
-  "53" },
+  "53"},
       :matriarch => {name: "Lady Montague", age:
-  "54" },
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+  "54"},
+      :hero => {name: "Romeo", age: "15", status:
+  "alive"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude:
-         "worried"},
+  "worried"},
          {name: "Mercutio", age: "18", attitude:
-         "hot-headed"}
+  "hot-headed"}
        ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age:
- "50" },
+ "50"},
       :matriarch => {name: "Lady Capulet", age:
- "51" },
+ "51"},
       :heroine => {name: "Juliet", age: "15",
- status: "alive" },
+ status: "alive"},
       :heroine_friends => [
         {name: "Steven", age: "30", attitude:
-"confused" },
+"confused"},
         {name: "Nurse", age: "44", attititude:
 "worried"}
       ]
